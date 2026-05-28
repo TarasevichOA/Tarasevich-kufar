0d9edf8ed2a01f417ac56806c6e71346a261df86
TarasevichOA <tarasevicholga2014@gmail.com>
2026-05-28 21:51:28 +0300

    Change xPath on HomePage.

--- src/main/java/by/kufar/HomePage.java
+++ src/main/java/by/kufar/HomePage.java
@@ -17,11 +17,12 @@ public class HomePage {
     private static final Logger logger = LogManager.getLogger(HomePage.class);
     private final String URL = "https://www.kufar.by/";
     private final String INPUT_SEARCH = "//input[@placeholder = 'Поиск объявлений']";
-    private final String BUTTON_AUTH = "//button[contains(text(), 'Войти')]";
+    private final String BUTTON_AUTH = "//button[contains(@class, 'styles_button') and contains(@class, 'styles_outline')]";
     private final String BUTTON_SEARCH = "//button[@class='styles_search_button__Ro1wM']";
     private final String COPY_RIGHTS = "//*[@id=\"footer-container\"]/div[3]/div[1]/p[1]";
     private final String COOKIES = "//*[@id=\"__next\"]/div[2]/div/div/div/div/button[2]";
-    private final String EMPTY_RESULT = "//*[contains(text(), 'По вашему запросу нет точных совпадений, но мы подобрали похожие варианты')]";
+    private final String EMPTY_RESULT = "//*[contains(text(), 'Мы это не нашли')]";
+    private final String EMPTY_RESULT2 = "//*[contains(text(), 'По вашему запросу нет точных совпадений, но мы подобрали похожие варианты')]";
 
     public HomePage() {
         super();
@@ -41,11 +42,8 @@ public class HomePage {
     }
 
     public String getButtonAuthText() {
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-
-        String buttonAuthText = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath(BUTTON_AUTH))).getText();
-        logger.info("Button Auth text is:{}", buttonAuthText);
+        String buttonAuthText = Driver.getDriver().findElement(By.xpath(BUTTON_AUTH)).getText();
+        logger.info("Button Auth {}", buttonAuthText);
         return buttonAuthText;
     }
 
@@ -73,6 +71,11 @@ public class HomePage {
     public String getEmptyResultMessage() {
         WebElement getEmptyResultMessage = WaitManager.getWait().until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath(EMPTY_RESULT)));
+        return getEmptyResultMessage.getText();
+    }
+    public String getEmptyResultMessage2() {
+        WebElement getEmptyResultMessage = WaitManager.getWait().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath(EMPTY_RESULT2)));
         return getEmptyResultMessage.getText();
     }
 
--- src/test/java/by/kufar/HomeTest.java
+++ src/test/java/by/kufar/HomeTest.java
@@ -41,10 +41,20 @@ public class HomeTest extends BaseTest {
     @DisplayName("Checking text in Search field when User can't find any product")
     @Test
     public void testNothingFound() {
-        String searchQuery = "ttttttttttttttttttt";
+        String searchQuery = "eeeereeterteyery";
         homePage.clickCookies();
         homePage.fillInputSearch(searchQuery);
         homePage.clickButtonSearch();
-        Assertions.assertEquals("По вашему запросу нет точных совпадений, но мы подобрали похожие варианты", homePage.getEmptyResultMessage());
+        Assertions.assertEquals("Мы это не нашли", homePage.getEmptyResultMessage());
+    }
+
+    @DisplayName("Checking text in Search field when User can't find any product")
+    @Test
+    public void testNothingFound2() {
+        String searchQuery = "cattttttttttt";
+        homePage.clickCookies();
+        homePage.fillInputSearch(searchQuery);
+        homePage.clickButtonSearch();
+        Assertions.assertEquals("По вашему запросу нет точных совпадений, но мы подобрали похожие варианты", homePage.getEmptyResultMessage2());
     }
 }
