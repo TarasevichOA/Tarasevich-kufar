9a7f51550a660af0ba54f29326029caaef9ffaa9
TarasevichOA <tarasevicholga2014@gmail.com>
2026-06-16 14:50:39 +0300

    Delete createAllureEnvironmentFile() to BaseTest.

--- src/test/java/by/kufar/BaseTest.java
+++ src/test/java/by/kufar/BaseTest.java
@@ -4,46 +4,18 @@ import by.kufar.driver.Driver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 @Execution(ExecutionMode.CONCURRENT)
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public abstract class BaseTest {
     private static final Logger logger = LogManager.getLogger(BaseTest.class);
     protected HomePage homePage;
-
-    @BeforeAll
-    public static void createAllureEnvironmentFile() {
-        try {
-            // Динамически берём путь к allure-results из системы (учитывает target)
-            String resultsDirPath = System.getProperty("allure.results.directory", "target/allure-results");
-            File allureResultsDir = new File(resultsDirPath);
-
-            if (!allureResultsDir.exists()) {
-                allureResultsDir.mkdirs();
-            }
-
-            Properties properties = new Properties();
-            properties.setProperty("Browser", "Cross-Browser (Chrome, Firefox, Edge)");
-            properties.setProperty("Environment", "Jenkins CI");
-            properties.setProperty("Threads", "3 (Fixed Strategy)");
-            properties.setProperty("URL", "https://kufar.by");
-
-            // Записываем файл строго в целевую папку результатов
-            FileOutputStream fos = new FileOutputStream(new File(allureResultsDir, "environment.properties"));
-            properties.store(fos, "Allure Environment Properties");
-            fos.close();
-        } catch (IOException e) {
-            System.err.println("Не удалось создать файл environment.properties: " + e.getMessage());
-        }
-    }
 
     @BeforeEach
     public void setupAuth(TestInfo testInfo) {
